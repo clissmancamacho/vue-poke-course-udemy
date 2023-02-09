e67fc71e76bb907c0f622f92310c96c349db2774
Clissman J. Camacho <clissmancamacho@gmail.com>
2023-02-09 10:12:26 -0300

    store with api options

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
+<template>
+  <div>
+    <h1>Home Counter: {{ useCounter.count }}</h1>
+    <h1>Double: {{ useCounter.double }}</h1>
+    <button @click="useCounter.increment">Incrementar</button>
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { useCounterStore } from '@/store/counter';
+
+const useCounter = useCounterStore()
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style lang="scss" scoped>
+
+</style>
